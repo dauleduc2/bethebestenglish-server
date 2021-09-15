abdf26b6fb5135487fd6941af1df0dacf7a1af4a
Duc Dau <dauleduc2@gmail.com>
2021-09-16 02:04:15 +0700

    first commit

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const databaseName = "bethebestenglish";
 module.exports = function () {
+    // mongoose
+    //     .connect(`mongodb://${process.env.dataUrl}/${databaseName}`, {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true,
+    //     })
+    //     .then(() => console.log("connected to mongodb...."));
+
     mongoose
-        .connect(`mongodb://${process.env.dataUrl}/${databaseName}`, {
+        .connect(`${process.env.dataUrl}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
