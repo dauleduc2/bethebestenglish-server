cbe8b031da3eb8bd23877f0eb3e67513be20deac
Duc Dau <dauleduc2@gmail.com>
2021-09-27 15:53:24 +0700

    ADD - remove course

--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
 const databaseName = "bethebestenglish";
 module.exports = function () {
-    // mongoose
-    //     .connect(`mongodb://${process.env.dataUrl}/${databaseName}`, {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true,
-    //     })
-    //     .then(() => console.log("connected to mongodb...."));
-
     mongoose
         .connect(`${process.env.dataUrl}`, {
             useNewUrlParser: true,
